9a4e3f7c28ad740617af9fb9e7d92e170d6129e2
gridsyntax-admin <gridsyntax@gmail.com>
2025-11-26 17:00:57 -0800

    Local Storage set to remember users' theme choice. Project COMPLETED!

--- script.js
+++ script.js
@@ -32,11 +32,25 @@ function lightMode() {
 function switchTheme(event) {
   if(event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
-toggleSwitch.addEventListener('change', switchTheme)
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local storage for local theme
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
